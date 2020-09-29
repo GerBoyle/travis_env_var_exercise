18930f103f0071574fad2b99f95c47aab20f7c8f
Gerard Boyle <ger_boyle@yahoo.ie>
2020-09-29 17:25:20 +0100

    Updated server.js file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
+const favoriteFood = process.env.FAVORITE_FOOD;
+
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    const favoriteFood = process.env.FAVORITE_FOOD;
     console.log("Microservices rock!");
     console.log(`My favorite food is ${favoriteFood}`)
     await sleep(5000);
